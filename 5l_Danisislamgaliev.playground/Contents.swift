b3653a9edaf966319b2b68a71911a2923ce21365
islamgaliev <danisislamgaliev@gmail.com>
2019-08-02 16:16:14 +0300

    lesson5_1

--- 5l_Danisislamgaliev.playground/Contents.swift
+++ 5l_Danisislamgaliev.playground/Contents.swift
@@ -1,3 +1,44 @@
 import UIKit
 
-var str = "Hello, playground"
+
+protocol Car {
+    
+    var label: String {get}
+    var Color: String {get set}
+    var type: String {get set}
+    var speed: Int {get}
+    
+    func getTypeCar()
+    func getSpeedCar()
+}
+
+
+class CarType: Car {
+    
+    var label: String = "BMW"
+    var Color: String = "black"
+    var type: String = "Big"
+    var speed: Int = 300
+    
+    
+    func getTypeCar() {
+        print("тип машины: \(type)")
+    }
+    
+    func getSpeedCar() {
+        print("скорость машины: \(speed)")
+    }
+    
+    
+}
+
+extension CarType {
+    
+}
+
+let car1 = CarType()
+car1.getSpeedCar()
+car1.getTypeCar()
+
+
+
