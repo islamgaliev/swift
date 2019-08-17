20a073b02182b05961c21b791c3e44c6ad83d094
islamgaliev <danisislamgaliev@gmail.com>
2019-08-17 18:23:04 +0300

    interfase_lesson1_3

---
+++ edtech/edtech/Controllers/AllCitiesVC/AllCitiesVC.swift
@@ -0,0 +1,42 @@
+//
+//  AllCitiesVC.swift
+//  edtech
+//
+//  Created by Kuku Kuku on 17/08/2019.
+//  Copyright © 2019 Kuku Kuku. All rights reserved.
+//
+
+import UIKit
+
+class AllCitiesVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var cities: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    @IBAction func AddCityButton(_ sender: Any) {
+        performSegue(withIdentifier: "ToSelect", sender: nil)
+    }
+    
+}
+
+extension AllCitiesVC: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AllCityCell") as! CityCell
+        cell.setupWithCity(cities[indexPath.row])
+        return cell
+    }
+    
+}
